7bf25fe8d52a351cb3f6fac288a0c750241b0c91
josh fong <joshz.fong@gmail.com>
2022-11-18 00:42:15 -0600

    added mapRAM to RAMs sv

---
+++ Quartus/RAMs.sv
@@ -0,0 +1,51 @@
+module characterRAM
+(
+		input [23:0] data_In,
+		input [12:0] write_address, read_address,
+		input we, Clk,
+
+		output logic [4:0] data_Out
+);
+
+	// mem has width of 3 bits and a total of 400 addresses
+	logic [7:0] mem [0:6612];
+
+	initial
+	begin
+		 $readmemh("Sprites/Ethan.txt", mem);
+	end
+
+
+	always_ff @ (posedge Clk) begin
+		if (we)
+			mem[write_address] <= data_In;
+		data_Out<= mem[read_address];
+	end
+
+endmodule
+
+module mapRAM
+(
+		input [23:0] data_In,
+		input [20:0] write_address, read_address,
+		input we, Clk,
+
+		output logic [4:0] data_Out
+);
+
+	// mem has width of 3 bits and a total of 400 addresses
+	logic [7:0] mem [0:1179648];
+
+	initial
+	begin
+		 $readmemh("Sprites/map_violet-city.txt", mem);
+	end
+
+
+	always_ff @ (posedge Clk) begin
+		if (we)
+			mem[write_address] <= data_In;
+		data_Out<= mem[read_address];
+	end
+
+endmodule
