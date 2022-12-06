aeec3c7f66d03a4e2c0b7994e7f8a4aaeda4bd03
josh fong <joshz.fong@gmail.com>
2022-12-06 17:16:40 -0600

    collision fixed ???

--- ECE385-HelperTools-master/PNG To Hex/On-Chip Memory/scripts/collision_setting_custom.py
+++ ECE385-HelperTools-master/PNG To Hex/On-Chip Memory/scripts/collision_setting_custom.py
@@ -4,34 +4,56 @@ filename = input("File name in sprite_originals (eg. map_violet-city-medres-coll
 width = int(input("First dimension (ex. 320): "))
 height = int(input("Second dimension (ex. 240): "))
 widthchar = int(input("Character width (ex. 19): "))
-heightchar = int(input("Character height (ex. 28): "))
+heightchar = int(input("Character height (ex. 29): "))
 
-fileAsArr = np.zeros((width, height))
-ret = np.zeros((width, height, 4)) # 0 - up, 2 - down, 1 - left, 3 - right
+fileAsArr = np.zeros((height, width))
+ret = np.zeros((height, width, 4)) # 0 - up, 2 - down, 1 - left, 3 - right
 with open("./testingthis/" + filename + ".txt", 'r') as f:
-  for i in range(width):
-    for j in range(height):
+  for i in range(height):
+    for j in range(width):
       fileAsArr[i][j] = int(f.readline())
 
 def setCollision(arr, x, y):
-   for i in range(widthchar):
-       if(x+i < width and fileAsArr[x+i][y] == 0):
-           arr[x][y][0] = 1
-       if(y+heightchar < height and x+i < width and fileAsArr[x+i][y+heightchar] == 0):
-           arr[x][y][2] = 1
-   for i in range(heightchar):
-       if(y+i < height and fileAsArr[x][y+i] == 0):
-           arr[x][y][1] = 1
-       if(x+widthchar < width and y+i < height and fileAsArr[x+widthchar][y+i] == 0):
-           arr[x][y][3] = 1
+    topRow = y-1
+    bottomRow = y+heightchar
+    leftCol = x-1
+    rightCol = x+widthchar
+    
+    if(topRow >= 0):
+        for i in range(widthchar):
+            if(x+i < width and fileAsArr[topRow][x+i] == 0):
+                arr[y][x][0] = 1
+    else:
+        arr[y][x][0] = 1
+    
+    if(bottomRow < height):
+        for i in range(widthchar):
+            if(x+i < width and fileAsArr[bottomRow][x+i] == 0):
+                arr[y][x][2] = 1
+    else:
+        arr[y][x][2] = 1
+
+    if(leftCol >= 0):
+        for i in range(heightchar):
+            if(y+i < height and fileAsArr[y+i][leftCol] == 0):
+                arr[y][x][1] = 1
+    else:
+        arr[y][x][1] = 1
+
+    if(rightCol < width):
+        for i in range(heightchar):
+            if(y+i < height and fileAsArr[y+i][rightCol] == 0):
+                arr[y][x][3] = 1
+    else:
+        arr[y][x][3] = 1
 
 
-for i in range(width):
-  for j in range(height):
-    setCollision(ret, i, j)
+for i in range(height):
+  for j in range(width):
+    setCollision(ret, j, i)
 
 f2 = open("./testingthis/mem__" + filename + ".txt", 'w')
-for i in range(width):
-  for j in range(height):
+for i in range(height):
+  for j in range(width):
     f2.write(f"{int(ret[i][j][0])}{int(ret[i][j][1])}{int(ret[i][j][2])}{int(ret[i][j][3])}\n")
 f2.close()
